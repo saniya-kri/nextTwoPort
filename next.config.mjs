531a6c4c3955564e4d93b4133dff3993e7b2c544
saniya-kri <anjali45528@gmail.com>
2024-09-23 23:28:39 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Ignore critical dependency warnings
+    config.ignoreWarnings = [
+      /critical dependency: the request of a dependency is an expression/,
+    ];
+    return config;
+  },
+};
 
 export default nextConfig;
